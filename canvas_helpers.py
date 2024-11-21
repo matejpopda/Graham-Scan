014b4d054dee91193171fe6d51e43fb68488144c
m-at-FNSPE <20053580+m-at-FNSPE@users.noreply.github.com>
2024-11-21 15:41:40 +0100

    Done

--- canvas_helpers.py
+++ canvas_helpers.py
@@ -25,24 +25,47 @@ class canvasWrapper:
         self.current_boundary: list[Line] = []
         self.lines_on_stack: list[Line] = []
         self.removed_lines: list[Line] = []
+        self.sorted: bool = False
+        self.position: int = 0
+        self.lowest_point: Point|None = None
     
     def draw(self):
         self.canvas.delete("all")
 
-        for point in self.points:
-            size = POINT_SIZE/2
-            self.canvas.create_oval(point.x - size, point.y - size, point.x + size, point.y + size)
+        def transform_y_coord(y):
+            return  y 
+            return self.canvas.winfo_reqheight() - y 
+
+        def draw_line(line: Line, dash=(1), fill="black"):
+            self.canvas.create_line(line.a.x, 
+                                    transform_y_coord(line.a.y), 
+                                    line.b.x, 
+                                    transform_y_coord(line.b.y), 
+                                    dash=dash, 
+                                    fill=fill)
+
+        def draw_point(point: Point, size):
+            x = point.x
+            y = transform_y_coord(point.y)
+
+            self.canvas.create_oval(x - size, 
+                                    y - size, 
+                                    x + size, 
+                                    y + size)
+
+        for i in range(len(self.points)):
+            point = self.points[i]
+            size = POINT_SIZE/2 * (i+1)
+            draw_point(point, size)
 
         for line in self.current_boundary:
-            self.canvas.create_line(line.a.x, line.a.y, line.b.x, line.b.y, dash=(1,0), fill="black")
+            draw_line(line)
 
         for line in self.lines_on_stack:
-            self.canvas.create_line(line.a.x, line.a.y, line.b.x, line.b.y, dash=(1,0), fill="green")
+            draw_line(line, dash=(1), fill='green')
 
         for line in self.removed_lines:
-            self.canvas.create_line(line.a.x, line.a.y, line.b.x, line.b.y, dash=(1,2), fill="red")
-
-
+            draw_line(line, dash=(1,2), fill="red")
 
 
 def clear_canvas(canvas: canvasWrapper):
@@ -51,6 +74,9 @@ def clear_canvas(canvas: canvasWrapper):
     canvas.lines_on_stack = []
     canvas.removed_lines = []
     canvas.draw()
+    canvas.sorted = False
+    canvas.lowest_point = None
+    canvas.position = 0
 
 
 
@@ -59,8 +85,8 @@ def random_points(canvas: canvasWrapper):
     boundaryy = canvas.canvas.winfo_reqheight()
 
     for _ in range(10):
-        x = random.uniform(0, boundaryx)
-        y = random.uniform(0, boundaryy)
+        x = random.uniform(boundaryx*0.2, boundaryx*0.8)
+        y = random.uniform(boundaryy*0.2, boundaryy*0.8)
 
         canvas.points.append(Point(x,y))
 
@@ -68,20 +94,79 @@ def random_points(canvas: canvasWrapper):
 
 
 
+def graham_step_no_draw(canvas: canvasWrapper) -> bool:
+    if not canvas.sorted:
+        find_lowest_point(canvas)
+        def sorter(point: Point):
+            if canvas.lowest_point == point:
+                return -100
+            x = point.x - canvas.lowest_point.x
+            y = point.y - canvas.lowest_point.y
+            res = (1 * x + 0 * y)/((1)*(pow(x*x+y*y, 1/2)))
+            # print(res)
+            return res 
+            
+        canvas.points.sort(key=sorter, reverse=False)
+        canvas.sorted = True
+        canvas.position = 2
+
+        canvas.lines_on_stack.append(Line(canvas.points[0], canvas.points[1]))
+        return False
+
+
+    if canvas.position == len(canvas.points):
+        canvas.lines_on_stack.append(Line(canvas.points[-1], canvas.points[0]))
+        return True    
+    
+    if canvas.position > len(canvas.points)- 1 :
+        return True
+
+    current_point = canvas.points[canvas.position]
+
+    canvas.lines_on_stack.append(Line(canvas.lines_on_stack[-1].b, current_point))
+
+
+
+    if cross_product(canvas.lines_on_stack[-2], canvas.lines_on_stack[-1]) > 0:
+        canvas.removed_lines.append(canvas.lines_on_stack.pop())
+        canvas.removed_lines.append(canvas.lines_on_stack.pop())
+        return False
+
+    canvas.position += 1 
+    
+    return False
+
+def cross_product(line1:Line, line2:Line):
+    assert line1.b == line2.a 
+
+    return (line1.b.x - line1.a.x)*(line2.b.y - line1.a.y) - (line1.b.y - line1.a.y)*(line2.b.x - line1.a.x)
+    
+       
+
+    
+
+
+        
+
+def find_lowest_point(canvas: canvasWrapper) -> None:
+    lowest = canvas.points[0]
+
+    for i in canvas.points:
+        if i.y < lowest.y:
+            lowest = i
+
+    canvas.lowest_point= lowest
+
 def graham_step(canvas: canvasWrapper) -> bool:
-    pass
+    x = graham_step_no_draw(canvas)
+    canvas.draw()
+    return x 
 
-
-def graham_iterate(canvas: canvasWrapper):
-    while graham_step(canvas):
-        time.sleep(0.1)
 
 
 def graham_result(canvas: canvasWrapper):
-    while graham_step(canvas):
+    while not graham_step_no_draw(canvas):
         pass
+    canvas.draw()
 
 
-
-def convex_hull(canvas: canvasWrapper):
-    pass
--- main.py
+++ main.py
@@ -19,10 +19,18 @@ ttk.Separator(form, orient="vertical").grid(column=1, row=0, rowspan=10)
 
 wrapper = canvas_helpers.canvasWrapper(canvas)
 
+
+def graham_iterate():
+    print("AAAA")
+    if not canvas_helpers.graham_step(wrapper):
+        print("bb")
+        canvas.after(100, graham_iterate)
+
+
 ttk.Button(form, text="Add random points", command=lambda: canvas_helpers.random_points(wrapper)).grid(column=2, row=1)
-ttk.Button(form, text="Convex Hull", command=lambda: canvas_helpers.graham_result(wrapper)).grid(column=2, row=2)
-ttk.Button(form, text="Run Graham Sort", command=lambda: canvas_helpers.graham_iterate(wrapper)).grid(column=2, row=3)
-ttk.Button(form, text="Step through", command=lambda: canvas_helpers.graham_step(wrapper)).grid(column=2, row=4)
+ttk.Button(form, text="Finish Graham Sort", command=lambda: canvas_helpers.graham_result(wrapper)).grid(column=2, row=2)
+ttk.Button(form, text="Animate Graham Sort", command=lambda: graham_iterate()).grid(column=2, row=3)
+ttk.Button(form, text="Do 1 step", command=lambda: canvas_helpers.graham_step(wrapper)).grid(column=2, row=4)
 ttk.Button(form, text="Clear canvas", command=lambda: canvas_helpers.clear_canvas(wrapper)).grid(column=2, row=5)
 ttk.Button(form, text="Quit", command=window.destroy).grid(column=2, row=6)
 
